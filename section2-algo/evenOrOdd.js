87e684913fcbcae8697efdefde96bcb90bd9b567
chiragdoctor <chirag.doctor@gmail.com>
2020-04-09 15:40:29 +0100

    program to check if given input is  even or odd

---
+++ section2-algo/evenOrOdd.js
@@ -0,0 +1,8 @@
+// 1) Write a program to given input Check whether Even or odd.
+
+var evenOrOdd = function(input) {
+    return input % 2 === 0 ? 'even' : 'odd';
+}
+
+console.log(evenOrOdd(3)); //odd;
+console.log(evenOrOdd(100024)); //even;
