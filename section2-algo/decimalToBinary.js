9765754dfd38e13ec913204c9ea12addc09febca
chiragdoctor <chirag.doctor@gmail.com>
2020-04-16 21:37:11 +0100

    decimal to binary using loops

---
+++ section2-algo/decimalToBinary.js
@@ -0,0 +1,18 @@
+// 25) Write a program to Convert Decimal to Binary
+
+function binary(num) {
+    let binary = '';
+    let number = num;
+    while (number > 0) {
+        if(number % 2 === 0) {
+            binary = Math.floor(number % 2) + binary
+        } else {
+            binary = Math.floor(number % 2) + binary
+        }
+        number = Math.floor(number / 2);
+    }
+    return binary;
+}
+
+console.log(binary(10));
+console.log(binary(1024));
