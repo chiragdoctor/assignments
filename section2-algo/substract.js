d2f9c6e000a2dd51bf5f7a3c6daae9b3e9d00479
chiragdoctor <chirag.doctor@gmail.com>
2020-04-17 00:35:48 +0100

    subsrtact without minus operator

---
+++ section2-algo/substract.js
@@ -0,0 +1,27 @@
+// Write a program to subtract two integers without using Minus (-) operator
+
+function subtract(x , y) {
+     // Iterate till there 
+    // is no carry 
+    while (y != 0) 
+    { 
+        // borrow contains common  
+        // set bits of y and unset 
+        // bits of x 
+        const borrow = (~x) & y; 
+  
+        // Subtraction of bits of x  
+        // and y where at least one 
+        // of the bits is not set 
+        x = x ^ y; 
+  
+        // Borrow is shifted by one  
+        // so that subtracting it from 
+        // x gives the required sum 
+        y = borrow << 1; 
+    } 
+    return x; 
+}
+
+console.log(subtract(13, 16)); // -3 
+console.log(subtract(30, 16)); // -14 
