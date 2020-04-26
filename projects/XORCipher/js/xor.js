8a239fea344284516f851092c7a7245edb2605da
chiragdoctor <chirag.doctor@gmail.com>
2020-04-26 20:27:29 +0100

    add reset to both forms

--- projects/XORCipher/js/xor.js
+++ projects/XORCipher/js/xor.js
@@ -4,6 +4,11 @@ encryptForm.addEventListener('submit', cipher);
 const decryptForm = document.getElementById('decrypt');
 decryptForm.addEventListener('submit', cipher);
 
+const encryptResetButton = document.getElementById('encrypt-reset');
+encryptResetButton.addEventListener('click', resetClick);
+
+const decryptResetButton = document.getElementById('decrypt-reset');
+decryptResetButton.addEventListener('click', resetClick);
 
 function cipher(e){
     e.preventDefault();
@@ -21,3 +26,13 @@ function cipher(e){
     output.innerHTML = result.join("");
 }
 
+function resetClick(e) {
+    const type = e.srcElement.id.indexOf('encrypt') === 0 ? 'encrypt' : 'decrypt'; 
+    const input = document.getElementById(`${type}-input`);
+    const key = document.getElementById(`${type}-key`);
+    const output = document.getElementById(`${type}-output`);
+    
+    input.value = '';
+    key.value = '';
+    output.value = '';   
+}
