cf268728df6039433a3299da5c3c6baf868d7477
chiragdoctor <chirag.doctor@gmail.com>
2020-04-28 10:51:01 +0100

    remove console.log

--- projects/MineSweeper/basic/js/basic.js
+++ projects/MineSweeper/basic/js/basic.js
@@ -2,12 +2,9 @@ const container = document.getElementById('container');
 let table = '';
 
 function onCellClicked(e) {
-    console.log(e.target.textContent);
     const item = parseInt(e.target.textContent);
-    console.log('item', item)
     for(let i = 1; i <= 100; i++) {
         if(i % item === 0) {
-            console.log(i);
             const cell = document.getElementById(`cell${i}`);
             cell.innerHTML = '<b>**</b>';
             cell.style.backgroundColor = 'brown';
